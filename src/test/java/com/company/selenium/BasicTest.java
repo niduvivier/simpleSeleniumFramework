23ba0cee5d25b914ccf954ea9356ea964225208f
Nicolas Duvivier <nicosan92@gmail.com>
2020-07-24 15:40:15 +0200

    initial commit

---
+++ src/test/java/com/company/selenium/BasicTest.java
@@ -0,0 +1,18 @@
+package com.company.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicTest {
+
+    @Test
+    public void firstTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MrKin\\Documents\\aqua\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.be");
+        Assert.assertEquals("https://www.google.be/", driver.getCurrentUrl());
+        driver.close();
+    }
+}
